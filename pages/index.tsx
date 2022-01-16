44d063fb72e006b00947dfaa93337c59010a31f0
Adan Abbi <adanweli@hotmail.com>
2022-01-16 13:47:27 -0800

    adan portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,61 +7,61 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>My Portfolio</title>
+        <title>Adan's Portfolio</title>
         <meta
-          name="description"
-          content="A portfolio of software developer work."
+          name='description'
+          content='A portfolio of software developer work.'
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>John Vandivier</h1>
+        <h1 className={styles.title}>Adan Abbi</h1>
         <p className={styles.description}>A Portfolio</p>
 
         <div className={styles.grid}>
           <a
-            href="https://github.com/Vandivier"
+            href='https://github.com/Adan206'
             className={styles.card}
-            rel="noreferrer"
-            target="_blank"
+            rel='noreferrer'
+            target='_blank'
           >
             <h2>GitHub</h2>
-            <p>Experience my passion for Open Source!</p>
+            <p>Welcome to my dev world</p>
           </a>
 
           <a
-            href="https://www.linkedin.com/in/john-vandivier"
+            href='https://www.linkedin.com/in/adan-abbi-57391848/'
             className={styles.card}
-            rel="noreferrer"
-            target="_blank"
+            rel='noreferrer'
+            target='_blank'
           >
             <h2>LinkedIn</h2>
-            <p>Connect with me professionally and learn about my background.</p>
+            <p>go from a web user to a web creator</p>
           </a>
 
           <a
-            href="https://www.afterecon.com/"
+            href='https://adan206.github.io/g-blog/'
             className={styles.card}
-            rel="noreferrer"
-            target="_blank"
+            rel='noreferrer'
+            target='_blank'
           >
             <h2>Blog</h2>
-            <p>Read my miscellaneous ramblings and learnings!</p>
+            <p>Sharing my dev experience with the world</p>
           </a>
 
-          <a
-            href="https://vandivier.github.io/vanilla-flat-white/"
+          {/* <a
+            href='https://vandivier.github.io/vanilla-flat-white/'
             className={styles.card}
-            rel="noreferrer"
-            target="_blank"
+            rel='noreferrer'
+            target='_blank'
           >
             <h2>Vanilla Flat White</h2>
             <p>
               An assortment of microprojects in HTML, CSS, and Vanilla
               JavaScript.
             </p>
-          </a>
+          </a> */}
         </div>
       </main>
 
