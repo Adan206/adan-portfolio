5d1891be607bf0809a0064380c76db918a008567
Adan Abbi <adanweli@hotmail.com>
2022-03-16 17:44:33 -0700

    Deploy Next.js to gh-pages

--- out/_next/static/AtdudM2pR8k6AzewArvcq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/68907ee90708f132d32d.css","static/chunks/pages/index-1d920284987f38f37843.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
