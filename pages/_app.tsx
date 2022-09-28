0cb209ce33cdfb38bfe41d204ac9755df52e8d16
Adan Abbi <adanweli@hotmail.com>
2022-09-28 14:58:39 -0700

    changed component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//       <Component {...pageProps} />
+
+//   );
+// }
+// export default MyApp;
+// ref: https://stackoverflow.com/a/71869550/11367370
+
+const MyApp = ({ Component, pageProps }: any) => (
+  <>
+    <Component {...pageProps} />
+  </>
+);
 export default MyApp;
